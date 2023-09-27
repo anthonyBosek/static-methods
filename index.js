925134f7c8fcf73a6ae5e964bca17785187a323b
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-27 06:28:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+
+  static titleize(string) {
+    let words = string.split(" ");
+    let title = [];
+    let exceptions = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    for (let i = 0; i < words.length; i++) {
+      if (i === 0) {
+        title.push(this.capitalize(words[i]));
+      } else {
+        if (exceptions.includes(words[i])) {
+          title.push(words[i]);
+        } else {
+          title.push(this.capitalize(words[i]));
+        }
+      }
+    }
+    return title.join(" ");
+  }
 }
